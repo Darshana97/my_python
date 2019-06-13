b732f491bf3e2c821b3526a4ecebce4ccac95f72
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-06-14 00:52:31 +0530

    arrays
    array in python

---
+++ Array.py
@@ -0,0 +1,11 @@
+x = int(input("enter the size of array: "))
+y=0;
+for y in range(x) :
+    y= int(input("enter values for array: "))
+    y=y+1
+
+arr=[x] 
+
+
+
+print(arr[0])
---
+++ Array2.py
@@ -0,0 +1,12 @@
+'''ch = input("enter the name: ")[0]
+
+print(ch)'''
+
+'''result = eval(input("enter the expression: "))
+print(result)'''
+
+from numpy import *
+
+arr = linspace(0,16)
+
+print(arr)
