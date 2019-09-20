1959e152f5c461eae2dd7ded81d5aea12927c302
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-09-20 21:44:45 +0530

    function
    function

---
+++ function.py
@@ -0,0 +1,28 @@
+
+'''def greet():
+    print("helloooo")
+    print("sri lanka")
+
+
+
+
+def add(x,y):
+    z=x+y
+    return z
+
+
+greet()
+result=add(1,9)
+
+print("value of z is: ",result)'''
+
+
+
+def add_sub(a,b):
+    c=a+b
+    d=a-b
+
+    return c,d
+
+result1=add_sub(15,3)
+print(result1)
